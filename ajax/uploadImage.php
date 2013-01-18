ab3a62e069b3b93cce0266e76f262098d3e5d41b
hasan <hasan@mm>
2013-01-18 15:31:06 +0200

    popular all categories bos ise gelmiyordu eklendi populer ilk acıldıgında loader geliyor o kaldırıldı folliwing menusu diasable edildi

--- ajax/uploadImage.php
+++ ajax/uploadImage.php
@@ -1,192 +1,234 @@
 <?php
+
 session_start();
 header("charset=utf8;Content-Type: text/html;");
 
-require_once __DIR__.'/../utils/Functions.php';
+require_once __DIR__ . '/../utils/Functions.php';
 
-$result=new Result();
-if(isset($_GET['type']) && $_GET['type']==1)
-{
-	$allowedExtensions = array("jpeg", "png", "jpg","gif"); 
-	$sizeLimit = 10 * 1024 * 1024;
+$result = new Result();
+if (isset($_GET['type']) && $_GET['type'] == 1) {
 
-	$uploader = new qqFileUploader($allowedExtensions, $sizeLimit);
+    $allowedExtensions = array("jpeg", "png", "jpg", "gif");
+    $sizeLimit = 10 * 1024 * 1024;
 
-	$result = $uploader->handleUpload(__DIR__.'/../uploads/',TRUE);
+    $uploader = new qqFileUploader($allowedExtensions, $sizeLimit);
 
-	echo htmlspecialchars(json_encode($result), ENT_NOQUOTES);
+    $result = $uploader->handleUpload(__DIR__ . '/../uploads/', TRUE);
+
+    $imgName = "";
+    if (isset($_GET['imageName'])) {
+        $imgName = $_GET['imageName'];
+    }
+    
+    $source_url=__DIR__ . '/../uploads/' . $imgName;
+    $info = getimagesize($source_url);
+  
+    if ($info['mime'] == 'image/jpeg')
+        $image = imagecreatefromjpeg($source_url);
+    elseif ($info['mime'] == 'image/gif')
+        $image = imagecreatefromgif($source_url);
+    elseif ($info['mime'] == 'image/png')
+        $image = imagecreatefrompng($source_url);
+
+    $size=filesize($source_url);
+    $quality=100;
+    if($size>=(100*1024) && $size<(512*1024))
+    {
+         $quality=60;
+    }
+    else if($size>=(512*1024) && $size<(1024*1024))
+    {
+        $quality=40;
+    }
+    else if($size>=(1024*1024) && $size<(3096*1024))
+    {
+        $quality=30;
+    }else
+    {
+        $quality=10;
+    }
+    echo $quality;
+    imagejpeg($image, $source_url, $quality);
+    echo htmlspecialchars(json_encode($result), ENT_NOQUOTES);
 }
-
 
 /**
  * Handle file uploads via XMLHttpRequest
  */
 class qqUploadedFileXhr {
-	/**
-	 * Save the file to the specified path
-	 * @return boolean TRUE on success
-	 */
-	function save($path) {
-		$input = fopen("php://input", "r");
-		$temp = tmpfile();
-		$realSize = stream_copy_to_stream($input, $temp);
-		fclose($input);
 
-		if ($realSize != $this->getSize()){
-			return false;
-		}
+    /**
+     * Save the file to the specified path
+     * @return boolean TRUE on success
+     */
+    function save($path) {
+        $input = fopen("php://input", "r");
+        $temp = tmpfile();
+        $realSize = stream_copy_to_stream($input, $temp);
+        fclose($input);
 
-		$target = fopen($path, "w");
-		fseek($temp, 0, SEEK_SET);
-		stream_copy_to_stream($temp, $target);
-		fclose($target);
+        if ($realSize != $this->getSize()) {
+            return false;
+        }
 
-		return true;
-	}
-	function getName() {
-            if(isset($_GET['imageName']))
-            {
-             return $_GET['imageName'];   
-            }else
-            {
-             return $_GET['qqfile'];   
-            }
-	}
-	function getSize() {
-		if (isset($_SERVER["CONTENT_LENGTH"])){
-			return (int)$_SERVER["CONTENT_LENGTH"];
-		} else {
-			throw new Exception('Getting content length is not supported.');
-		}
-	}
+        $target = fopen($path, "w");
+        fseek($temp, 0, SEEK_SET);
+        stream_copy_to_stream($temp, $target);
+        fclose($target);
+
+        return true;
+    }
+
+    function getName() {
+        if (isset($_GET['imageName'])) {
+            return $_GET['imageName'];
+        } else {
+            return $_GET['qqfile'];
+        }
+    }
+
+    function getSize() {
+        if (isset($_SERVER["CONTENT_LENGTH"])) {
+            return (int) $_SERVER["CONTENT_LENGTH"];
+        } else {
+            throw new Exception('Getting content length is not supported.');
+        }
+    }
+
 }
 
 /**
  * Handle file uploads via regular form post (uses the $_FILES array)
  */
 class qqUploadedFileForm {
-	/**
-	 * Save the file to the specified path
-	 * @return boolean TRUE on success
-	 */
-	function save($path) {
-		if(!move_uploaded_file($_FILES['qqfile']['tmp_name'], $path)){
-			return false;
-		}
-		return true;
-	}
-	function getName() {
-		return $_FILES['qqfile']['name'];
-	}
-	function getSize() {
-		return $_FILES['qqfile']['size'];
-	}
+
+    /**
+     * Save the file to the specified path
+     * @return boolean TRUE on success
+     */
+    function save($path) {
+        if (!move_uploaded_file($_FILES['qqfile']['tmp_name'], $path)) {
+            return false;
+        }
+        return true;
+    }
+
+    function getName() {
+        return $_FILES['qqfile']['name'];
+    }
+
+    function getSize() {
+        return $_FILES['qqfile']['size'];
+    }
+
 }
 
 class qqFileUploader {
-	private $allowedExtensions = array();
-	private $sizeLimit = 10485760;
-	private $file;
-	private $uploadName;
 
-	function __construct(array $allowedExtensions = array(), $sizeLimit = 10485760){
-		$allowedExtensions = array_map("strtolower", $allowedExtensions);
+    private $allowedExtensions = array();
+    private $sizeLimit = 10485760;
+    private $file;
+    private $uploadName;
 
-		$this->allowedExtensions = $allowedExtensions;
-		$this->sizeLimit = $sizeLimit;
+    function __construct(array $allowedExtensions = array(), $sizeLimit = 10485760) {
+        $allowedExtensions = array_map("strtolower", $allowedExtensions);
 
-		$this->checkServerSettings();
+        $this->allowedExtensions = $allowedExtensions;
+        $this->sizeLimit = $sizeLimit;
 
-		if (isset($_GET['qqfile'])) {
-			$this->file = new qqUploadedFileXhr();
-		} elseif (isset($_FILES['qqfile'])) {
-			$this->file = new qqUploadedFileForm();
-		} else {
-			$this->file = false;
-		}
-	}
+        $this->checkServerSettings();
 
-	public function getUploadName(){
-		if( isset( $this->uploadName ) )
-			return $this->uploadName;
-	}
+        if (isset($_GET['qqfile'])) {
+            $this->file = new qqUploadedFileXhr();
+        } elseif (isset($_FILES['qqfile'])) {
+            $this->file = new qqUploadedFileForm();
+        } else {
+            $this->file = false;
+        }
+    }
 
-	public function getName(){
-		if ($this->file)
-			return $this->file->getName();
-	}
+    public function getUploadName() {
+        if (isset($this->uploadName))
+            return $this->uploadName;
+    }
 
-	private function checkServerSettings(){
-		$postSize = $this->toBytes(ini_get('post_max_size'));
-		$uploadSize = $this->toBytes(ini_get('upload_max_filesize'));
+    public function getName() {
+        if ($this->file)
+            return $this->file->getName();
+    }
 
-		if ($postSize < $this->sizeLimit || $uploadSize < $this->sizeLimit){
-			$size = max(1, $this->sizeLimit / 1024 / 1024) . 'M';
-			die("{'error':'increase post_max_size and upload_max_filesize to $size'}");
-		}
-	}
+    private function checkServerSettings() {
+        $postSize = $this->toBytes(ini_get('post_max_size'));
+        $uploadSize = $this->toBytes(ini_get('upload_max_filesize'));
 
-	private function toBytes($str){
-		$val = trim($str);
-		$last = strtolower($str[strlen($str)-1]);
-		switch($last) {
-			case 'g': $val *= 1024;
-			case 'm': $val *= 1024;
-			case 'k': $val *= 1024;
-		}
-		return $val;
-	}
+        if ($postSize < $this->sizeLimit || $uploadSize < $this->sizeLimit) {
+            $size = max(1, $this->sizeLimit / 1024 / 1024) . 'M';
+            die("{'error':'increase post_max_size and upload_max_filesize to $size'}");
+        }
+    }
 
-	/**
-	 * Returns array('success'=>true) or array('error'=>'error message')
-	 */
-	function handleUpload($uploadDirectory, $replaceOldFile = FALSE){
-		if (!is_writable($uploadDirectory)){
-			return array('error' => "Server error. Upload directory isn't writable.");
-		}
+    private function toBytes($str) {
+        $val = trim($str);
+        $last = strtolower($str[strlen($str) - 1]);
+        switch ($last) {
+            case 'g': $val *= 1024;
+            case 'm': $val *= 1024;
+            case 'k': $val *= 1024;
+        }
+        return $val;
+    }
 
-		if (!$this->file){
-			return array('error' => 'No files were uploaded.');
-		}
+    /**
+     * Returns array('success'=>true) or array('error'=>'error message')
+     */
+    function handleUpload($uploadDirectory, $replaceOldFile = FALSE) {
+        if (!is_writable($uploadDirectory)) {
+            return array('error' => "Server error. Upload directory isn't writable.");
+        }
 
-		$size = $this->file->getSize();
+        if (!$this->file) {
+            return array('error' => 'No files were uploaded.');
+        }
 
-		if ($size == 0) {
-			return array('error' => 'File is empty');
-		}
+        $size = $this->file->getSize();
 
-		if ($size > $this->sizeLimit) {
-			return array('error' => 'File is too large');
-		}
+        if ($size == 0) {
+            return array('error' => 'File is empty');
+        }
 
-		$pathinfo = pathinfo($this->file->getName());
-		$filename = $pathinfo['filename'];
-		//$filename = md5(uniqid());
-		$ext = @$pathinfo['extension'];		// hide notices if extension is empty
+        if ($size > $this->sizeLimit) {
+            return array('error' => 'File is too large');
+        }
 
-		if($this->allowedExtensions && !in_array(strtolower($ext), $this->allowedExtensions)){
-			$these = implode(', ', $this->allowedExtensions);
-			return array('error' => 'File has an invalid extension, it should be one of '. $these . '.');
-		}
+        $pathinfo = pathinfo($this->file->getName());
+        $filename = $pathinfo['filename'];
+        //$filename = md5(uniqid());
+        $ext = @$pathinfo['extension'];  // hide notices if extension is empty
 
-		$ext = ($ext == '') ? $ext : '.' . $ext;
+        if ($this->allowedExtensions && !in_array(strtolower($ext), $this->allowedExtensions)) {
+            $these = implode(', ', $this->allowedExtensions);
+            return array('error' => 'File has an invalid extension, it should be one of ' . $these . '.');
+        }
 
-		if(!$replaceOldFile){
-			/// don't overwrite previous files that were uploaded
-			while (file_exists($uploadDirectory . $filename . $ext)) {
-				$filename .= rand(10, 99);
-			}
-		}
+        $ext = ($ext == '') ? $ext : '.' . $ext;
 
-		$this->uploadName = $filename . $ext;
+        if (!$replaceOldFile) {
+            /// don't overwrite previous files that were uploaded
+            while (file_exists($uploadDirectory . $filename . $ext)) {
+                $filename .= rand(10, 99);
+            }
+        }
 
-		if ($this->file->save($uploadDirectory . $filename . $ext)){
-			return array('success'=>true);
-		} else {
-			return array('error'=> 'Could not save uploaded file.' .
-					'The upload was cancelled, or server error encountered');
-		}
+        $this->uploadName = $filename . $ext;
 
-	}
+        if ($this->file->save($uploadDirectory . $filename . $ext)) {
+            return array('success' => true);
+        } else {
+            return array('error' => 'Could not save uploaded file.' .
+                'The upload was cancelled, or server error encountered');
+        }
+    }
+
 }
+
 ?>
--- test/test_util.php
+++ test/test_util.php
@@ -2,13 +2,15 @@
 session_start();
 header("Content-Type: text/html; charset=utf8");
 
-require_once __DIR__.'/../utils/Functions.php';
-
-
-$n=new Neo4jFuctions();
-$n->addUserInfo(6618310, "Hasan", "Keklik", USER_TYPE_NORMAL, "keklikhasan");
-
-//var_dump(Neo4jUserSettingsUtil::getUserSubscribeCategory(1000006,146));
-
-
+require_once __DIR__ . '/../utils/Functions.php';
 ?>
+
+
+<body>
+    <img src="<?= HOSTNAME ?>getImage.php?src=timety/uploads/ImageEventHeader20_9413733.png&w=494"></img>
+    <p/>
+    <img src="<?= HOSTNAME ?>getImage.php?src=http://timety.com/beta/uploads/events/1000076/ImageEventHeader24_5467344.png&h=54&w=64"></img>
+    <p/>
+    <img src="http://timety.com/beta/uploads/events/1000076/ImageEventHeader24_5467344.png"></img>
+    <p/>
+</body>
