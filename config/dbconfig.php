1b1afb46c5e9331f732fc9ee381791739ebe737f
hasan <hasan@mm>
2013-02-04 21:35:00 +0200

    blur de edgeler sharp edildi

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -2,6 +2,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
+//ebYU35198.@!1t
 define('DB_DATABASE', 'timete');
 $connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die(mysql_error());
 $database = mysql_select_db(DB_DATABASE) or die(mysql_error());
