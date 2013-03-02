692f81d132922021913ccc38b133fa8e8f72f71a
hasan <hasan@mm>
2013-03-02 15:23:24 +0200

    add quick event

--- utils/RedisFunctions.php
+++ utils/RedisFunctions.php
@@ -38,6 +38,47 @@ class RedisUtils {
         return $result;
     }
 
+    public static function getUpcomingEventsForUser($userId = -1, $pageNumber = 0, $pageItemCount = 50, $date = null, $query = null, $all = 1) {
+        $log = KLogger::instance(KLOGGER_PATH, KLogger::DEBUG);
+        if (!empty($userId)) {
+            if ($all == 0) {
+                //subscribed catagory
+            } else {
+                // all
+            }
+            if (empty($date)) {
+                $date = time();
+            }
+            $redis = new Predis\Client();
+            //$log->logInfo("RedisUtils > getUpcomingEvents > start");
+            $pgStart = $pageNumber * $pageItemCount;
+            $pgEnd = $pgStart + $pageItemCount - 1;
+            //$log->logInfo("RedisUtils > getUpcomingEvents > index " . $pgStart . " end " . $pgEnd);
+            $events = $redis->zrangebyscore(REDIS_PREFIX_USER.$userId.REDIS_SUFFIX_UPCOMING, $date, "+inf");
+            //$log->logInfo("RedisUtils > getUpcomingEvents > size " . sizeof($events));
+            $result = "[";
+            for ($i = 0; $i < sizeof($events); $i++) {
+                if ($i >= $pgStart && $i <= $pgEnd) {
+                    try {
+                        $r = ",";
+                        if ($i == $pgStart) {
+                            $r = "";
+                        }
+                        $result = $result . $r . $events[$i];
+                    } catch (Exception $exc) {
+                        $log->logError("RedisUtils > getUpcomingEvents > $i Error : " . $exc->getTraceAsString());
+                    }
+                }
+            }
+            $result = $result . "]";
+            //$log->logInfo("RedisUtils > getUpcomingEvents > result  ");
+            return $result;
+        } else {
+            $log->logInfo("RedisUtils > getUpcomingEvents > userId null  ");
+            return null;
+        }
+    }
+
     public static function getFollowingEvents($userId = -1, $pageNumber = 0, $pageItemCount = 50, $date = null, $query = null, $all = 1) {
         if (empty($userId) || $userId < 0) {
             return "[]";
