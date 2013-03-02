a1121b0f67570c9e05402f5ea29bbc12bf352441
hasan <hasan@mm>
2013-03-02 18:53:37 +0200

    add quick event

--- admin/removeEventById.php
+++ admin/removeEventById.php
@@ -7,19 +7,35 @@ require_once __DIR__ . '/../utils/Functions.php';
 if (isset($_GET['eventId']) && !empty($_GET['eventId'])) {
     $id = $_GET['eventId'];
 
+    //delete fromneo4j
     try {
         Neo4jEventUtils::removeEventById($id);
     } catch (Exception $exc) {
         echo $exc->getTraceAsString();
     }
 
+    //delete from mysql
     $SQL = "DELETE  FROM " . TBL_EVENTS . " WHERE id=" . $id;
     mysql_query($SQL) or die(mysql_error());
-    
-    $SQL = "DELETE  FROM " . TBL_COMMENT. " WHERE event_id=" . $id;
+
+    $SQL = "DELETE  FROM " . TBL_COMMENT . " WHERE event_id=" . $id;
     mysql_query($SQL) or die(mysql_error());
-    
-    $SQL = "DELETE  FROM " . TBL_IMAGES. " WHERE eventId=" . $id;
+
+    $SQL = "DELETE  FROM " . TBL_IMAGES . " WHERE eventId=" . $id;
     mysql_query($SQL) or die(mysql_error());
+
+    //delete from redis
+    $redis = new Predis\Client();
+    $keys = $redis->keys("*");
+    foreach ($keys as $key) {
+        $events = $redis->zrevrange($key, 0, -1);
+        foreach ($events as $item) {
+            $evt = json_decode($item);
+            if ($evt->id == $id) {
+                RedisUtils::removeItem($redis, $key, $item);
+                break;
+            }
+        }
+    }
 }
 ?>
--- test/test.php
+++ test/test.php
@@ -2,11 +2,9 @@
 require_once __DIR__ . '/../utils/Functions.php';
 require_once __DIR__ . '/../utils/SettingFunctions.php';
 
-$eventID = "1000293";
-$event = new Event();
-$event = Neo4jEventUtils::getNeo4jEventById($eventID);
-var_dump($event);
-if ($event->privacy == 1 || $event->privacy == "1") {
+$redis = new Predis\Client();
+$keys=$redis->keys("*");
+foreach ($keys as $key){
     
 }
 ?>
