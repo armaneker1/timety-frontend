091cb2afb2ebe2fd37bd835eb0234093ae52a0eb
hasan <hasan@mm>
2013-03-06 12:24:36 +0200

    add quick event

---
+++ admin/patches/eventswithoutags.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+header("Content-Type: text/html; charset=utf8");
+
+require_once __DIR__ . '/../../utils/Functions.php';
+
+$eventList = EventUtil::getAllEvents();
+?>
+<h1>Events</h1>
+<table>
+    <tr>
+        <td style="width: 10px;"> # </td>
+        <td style="width: 80px;">Event Id</td>
+        <td style="width: 150px;">Event Title</td>
+        <td style="width: 80px;">Creator</td>
+        <td style="width: 80px;">Start Date</td>
+    </tr>
+
+    <?php
+    $event = new Event();
+    $i = 0;
+    foreach ($eventList as $event) {
+        $tags = Neo4jEventUtils::getEventTimetyTags($event->id);
+        if (empty($tags)) {
+            ?>
+            <tr>
+                <td>#<?= $i ?></td>
+                <td><?= $event->id ?></td>
+                <td><?= $event->title ?></td>
+                <td><?= $event->creatorId ?></td>
+                <td><?= $event->startDateTime ?></td>
+                <?php
+                foreach ($tags as $tag) {
+                    ?> 
+                    <td  style="width: 150px;text-align: right;"><?= $tag->name ?> (tag)</td>
+                <?php } ?>
+            </tr>
+        <?php
+          $i++;
+        }
+    }
+    ?>
+</table>
+
+
+
