91dcd8c6ab61f791e2f3185d17e0e03de22105d3
hasan <hasan@mm>
2013-03-28 17:24:49 +0200

    ie calısmaları yapıldı

--- timthumb-config.php
+++ timthumb-config.php
@@ -1,10 +1,10 @@
 <?php
+
 define('ALLOW_ALL_EXTERNAL_SITES', true);
 define('FILE_CACHE_ENABLED', true);
-define('FILE_CACHE_TIME_BETWEEN_CLEANS', 86400 );
-define('FILE_CACHE_MAX_FILE_AGE', 86400 );
+define('FILE_CACHE_TIME_BETWEEN_CLEANS', 86400);
+define('FILE_CACHE_MAX_FILE_AGE', 86400);
 define('FILE_CACHE_DIRECTORY', './cache');
-
-
-
+define('PNG_IS_TRANSPARENT', false);
+define('DEFAULT_ZC', 2);
 ?>
