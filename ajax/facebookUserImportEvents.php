2ed742bd44226e6d14a0fa3c6bb211a3d4533dd9
hasan <hasan@mm>
2013-04-01 21:27:01 +0300

    düzeltmeler

---
+++ ajax/facebookUserImportEvents.php
@@ -0,0 +1,41 @@
+<?php
+
+session_start();
+header("Content-Type: text/html; charset=utf8");
+
+require_once __DIR__ . '/../utils/Functions.php';
+
+$userId = null;
+if (isset($_GET["userId"]))
+    $userId = $_GET["userId"];
+if (isset($_POST["userId"]))
+    $userId = $_POST["userId"];
+
+if (!empty($userId)) {
+    $provider = UserUtils::getSocialProvider($userId, FACEBOOK_TEXT);
+    if (!empty($provider) && sizeof($provider) > 0) {
+        $provider = $provider[0];
+        $facebook = new Facebook(array(
+                    'appId' => FB_APP_ID,
+                    'secret' => FB_APP_SECRET,
+                    'cookie' => true
+                ));
+        $facebook->setAccessToken($provider->oauth_token);
+        $result = $facebook->api('/me/events');
+        if (!empty($result['data']) && sizeof($result['data']) > 0) {
+            $events = $result['data'];
+            foreach ($events as $event) {
+                echo "<h1>" . $event['name'] . " (" . $event['id'] . ")</h1>";
+                //var_dump($facebook->api('/'.$event['id']));
+
+                $params = array(
+                    'method' => 'fql.query',
+                    'query' => 'select name, pic_big,host, description, start_time, end_time, location, venue,privacy,ticket_uri from event where eid="' . $event['id'] . '"'
+                );
+
+               var_dump( $facebook->api($params));
+            }
+        }
+    }
+}
+?>
