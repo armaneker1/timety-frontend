3fe0bd2a448c24dd7e33c062b984fa0924535856
hasan <hasan@mm>
2013-04-04 19:28:55 +0300

    add like fln

--- layout/layout_header.php
+++ layout/layout_header.php
@@ -31,7 +31,7 @@ if (stripos($br, 'MSIE')) {
 
 
 <!-- CSS -->
-<link href="<?= HOSTNAME ?>common.css?2012233255844" rel="stylesheet">
+<link href="<?= HOSTNAME ?>common.css?2012233255855" rel="stylesheet">
 <link  href="<?= HOSTNAME ?>resources/styles/slide.css" rel="stylesheet" type="text/css" />
 <link  href="<?= HOSTNAME ?>resources/styles/modalpanel.css?77" rel="stylesheet" type="text/css" />
 <link  href="<?= HOSTNAME ?>resources/styles/message.css" rel="stylesheet" type="text/css" />
