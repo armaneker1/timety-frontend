6be8bc43deb978867cbe6181100b8f8fe2942caa
hasan <hasan@mm>
2013-04-04 12:08:45 +0300

    add like fln

--- user.php
+++ user.php
@@ -83,8 +83,8 @@ if (isset($_GET['userId']) && !empty($_GET['userId'])) {
     exit(1);
 }
 ?>
-<!DOCTYPE html "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<!DOCTYPE HTML>
+<html dir="ltr" lang="en-US" xmlns:fb="http://www.facebook.com/2008/fbml" xmlns:og="http://opengraphprotocol.org/schema/">
     <head>
 
         <?php
