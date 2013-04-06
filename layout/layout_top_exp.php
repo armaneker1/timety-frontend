dd8a698bc6f528367c053f16fc4c2104e492bae1
hasan <hasan@mm>
2013-04-06 17:06:28 +0300

    password protection

--- layout/layout_top_exp.php
+++ layout/layout_top_exp.php
@@ -157,7 +157,7 @@ if (empty($user)) {
                 });
             </script>
             <script language="javascript" src="<?= HOSTNAME ?>resources/scripts/notification.min.js?20135879133"></script>
-            <script language="javascript" src="<?= HOSTNAME ?>resources/scripts/top_menu_popular.min.js?201308089733"></script>
+            <script language="javascript" src="<?= HOSTNAME ?>resources/scripts/top_menu_popular.min.js?201308089744"></script>
             <?php
         }
     }
