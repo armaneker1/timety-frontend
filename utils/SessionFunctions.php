1a3a4f7afe3255125e6ac96ab8f3a64c9486697d
hasan <hasan@mm>
2013-04-08 12:01:07 +0300

    password protection

--- utils/SessionFunctions.php
+++ utils/SessionFunctions.php
@@ -4,6 +4,24 @@ class SessionUtil {
 
     public static function checkNotLoggedinUser() {
         if (isset($_SESSION['id'])) {
+            $user = new User();
+            $user = UserUtils::getUserById($_SESSION['id']);
+        } else {
+            //check cookie
+            $rmm = false;
+            if (isset($_COOKIE[COOKIE_KEY_RM]))
+                $rmm = $_COOKIE[COOKIE_KEY_RM];
+            if ($rmm && isset($_COOKIE[COOKIE_KEY_UN]) && isset($_COOKIE[COOKIE_KEY_PSS])) {
+                $uname = base64_decode($_COOKIE[COOKIE_KEY_UN]);
+                $upass = base64_decode($_COOKIE[COOKIE_KEY_PSS]);
+                if (!empty($uname) && !empty($upass)) {
+                    $user = UserUtils::login($uname, $upass);
+                    if (!empty($user))
+                        $_SESSION['id'] = $user->id;
+                }
+            }
+        }
+        if (isset($_SESSION['id'])) {
             $user = UserUtils::getUserById($_SESSION['id']);
             if (!empty($user) && !empty($user->id) && !empty($user->userName)) {
                 header("location: " . HOSTNAME);
