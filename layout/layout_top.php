738630609e738c6356639a602250e615da494529
hasan <hasan@mm>
2013-04-08 15:27:25 +0300

    password protection

--- layout/layout_top.php
+++ layout/layout_top.php
@@ -1,5 +1,5 @@
 <?php
-if (!empty($user) && ($user->id == 6618346 || $user->id == 6618344 || $user->id == 6618370)) {
+if (true) {
     include('layout/layout_top_exp.php');
 } else {
     $user = null;
