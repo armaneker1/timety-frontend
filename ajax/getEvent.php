fdc3f70eeefd67d93d808e2de0f99ae4e15d5529
hasan <hasan@mm>
2013-04-08 15:43:14 +0300

    password protection

--- ajax/getEvent.php
+++ ajax/getEvent.php
@@ -1,37 +1,40 @@
 <?php
+
 session_start();
 header("charset=utf8;");
 
-require_once __DIR__.'/../utils/Functions.php';
+require_once __DIR__ . '/../utils/Functions.php';
 
-$eventId=null;
-if(isset($_POST["eventId"]))
-    $eventId=$_POST["eventId"];
-if(isset($_GET["eventId"]))
-    $eventId=$_GET["eventId"];
+$eventId = null;
+if (isset($_POST["eventId"]))
+    $eventId = $_POST["eventId"];
+if (isset($_GET["eventId"]))
+    $eventId = $_GET["eventId"];
 
-    
-$res=new Result();
-$res->error=true;
-$res->success=false;
 
-if(!empty($eventId))
-{
-   $result=Neo4jEventUtils::getEventFromNode($eventId,TRUE);
-   if(!empty($result))
-   {
-       $result->getHeaderImage();
-       $json_response = json_encode($result);
-       echo $json_response;
-   }else
-   { 
-       $json_response = json_encode($res);
-       echo $json_response;
-   }
-}else
-{ 
+$res = new Result();
+$res->error = true;
+$res->success = false;
+
+if (!empty($eventId)) {
+    $result = Neo4jEventUtils::getEventFromNode($eventId, TRUE);
+    if (!empty($result)) {
+        $result->getHeaderImage();
+        $result->images = array();
+        $result->getAttachLink();
+        $result->getTags();
+        $result->getLocCity();
+        $result->getWorldWide();
+        $result->attendancecount = Neo4jEventUtils::getEventAttendanceCount($eventId);
+        $result->commentCount = CommentUtil::getCommentListSizeByEvent($eventId, null);
+        $json_response = json_encode($result);
+        echo $json_response;
+    } else {
+        $json_response = json_encode($res);
+        echo $json_response;
+    }
+} else {
     $json_response = json_encode($res);
     echo $json_response;
 }
-
 ?>
