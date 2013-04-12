e8ad35b6e4268642468d829325dfdd3394774d97
hasan <hasan@mm>
2013-04-12 11:21:24 +0300

    password protection

--- utils/SessionFunctions.php
+++ utils/SessionFunctions.php
@@ -2,6 +2,79 @@
 
 class SessionUtil {
 
+    public static function checkLoggedinUser($checkStatus = true) {
+        if (isset($_SESSION['id'])) {
+            $user = new User();
+            $user = UserUtils::getUserById($_SESSION['id']);
+            if (!empty($user)) {
+                if ($checkStatus)
+                    SessionUtil::checkUserStatus($user);
+                return $user;
+            }
+        } else {
+            //check cookie
+            $rmm = false;
+            if (isset($_COOKIE[COOKIE_KEY_RM]))
+                $rmm = $_COOKIE[COOKIE_KEY_RM];
+            if ($rmm && isset($_COOKIE[COOKIE_KEY_UN]) && isset($_COOKIE[COOKIE_KEY_PSS])) {
+                $timeHash = base64_decode($_COOKIE[COOKIE_KEY_UN]);
+                $clientGuid = base64_decode($_COOKIE[COOKIE_KEY_PSS]);
+                if (!empty($timeHash) && !empty($clientGuid)) {
+                    $user = UserUtils::cookieLogin($timeHash, $clientGuid);
+                    if (!empty($user)) {
+                        $_SESSION['id'] = $user->id;
+                        if ($checkStatus)
+                            SessionUtil::checkUserStatus($user);
+                        return $user;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public static function storeLoggedinUser($user, $cookie = true) {
+        if (!empty($user)) {
+            $_SESSION['id'] = $user->id;
+            if ($cookie) {
+                $cookie = new TimeteUserCookie();
+                $cookie->setTimeHash(strtotime("now"));
+                $cookie->setUserId($user->id);
+                $cookie->setClientGuid(SessionUtil::getClientGUID($user->id));
+                $cookie->insertIntoDatabase(DBUtils::getConnection());
+                setcookie(COOKIE_KEY_RM, true, time() + (365 * 24 * 60 * 60), "/");
+                setcookie(COOKIE_KEY_UN, base64_encode($cookie->getTimeHash()), time() + (365 * 24 * 60 * 60), "/");
+                setcookie(COOKIE_KEY_PSS, base64_encode($cookie->getClientGuid()), time() + (365 * 24 * 60 * 60), "/");
+            }
+        }
+    }
+
+    public static function getClientGUID($userId) {
+        $val = sha1($userId) . "," . $_SERVER['HTTP_HOST'] . "," . $_SERVER['HTTP_USER_AGENT'];
+        return sha1($val);
+    }
+
+    public static function deleteLoggedinUser() {
+        if (isset($_COOKIE[COOKIE_KEY_UN]) && isset($_COOKIE[COOKIE_KEY_PSS])) {
+            $timeHash = base64_decode($_COOKIE[COOKIE_KEY_UN]);
+            $clientGuid = base64_decode($_COOKIE[COOKIE_KEY_PSS]);
+            $SQL = "SELECT * FROM " . TBL_USER_COOKIE . " WHERE time_hash = '$timeHash' AND client_guid='$clientGuid'";
+            $cookie = TimeteUserCookie::findBySql(DBUtils::getConnection(), $SQL);
+            if (!empty($cookie)) {
+                $cookie = $cookie[0];
+                if (!empty($cookie)) {
+                    $cookie->deleteFromDatabase(DBUtils::getConnection());
+                }
+            }
+        }
+        unset($_SESSION['id']);
+        setcookie(COOKIE_KEY_RM, false, time() + (365 * 24 * 60 * 60), "/");
+        setcookie(COOKIE_KEY_UN, "", time() + (365 * 24 * 60 * 60), "/");
+        setcookie(COOKIE_KEY_PSS, "", time() + (365 * 24 * 60 * 60), "/");
+    }
+
+    /*  */
+
     public static function checkNotLoggedinUser() {
         if (isset($_SESSION['id'])) {
             $user = new User();
@@ -12,19 +85,15 @@ class SessionUtil {
             if (isset($_COOKIE[COOKIE_KEY_RM]))
                 $rmm = $_COOKIE[COOKIE_KEY_RM];
             if ($rmm && isset($_COOKIE[COOKIE_KEY_UN]) && isset($_COOKIE[COOKIE_KEY_PSS])) {
-                $uname = base64_decode($_COOKIE[COOKIE_KEY_UN]);
-                $upass = base64_decode($_COOKIE[COOKIE_KEY_PSS]);
-                if (!empty($uname) && !empty($upass)) {
-                    $user = UserUtils::login($uname, $upass);
-                    if (!empty($user))
+                $timeHash = base64_decode($_COOKIE[COOKIE_KEY_UN]);
+                $clientGuid = base64_decode($_COOKIE[COOKIE_KEY_PSS]);
+                if (!empty($timeHash) && !empty($clientGuid)) {
+                    $user = UserUtils::cookieLogin($timeHash, $clientGuid);
+                    if (!empty($user)) {
                         $_SESSION['id'] = $user->id;
+                        header("location: " . HOSTNAME);
+                    }
                 }
-            }
-        }
-        if (isset($_SESSION['id'])) {
-            $user = UserUtils::getUserById($_SESSION['id']);
-            if (!empty($user) && !empty($user->id) && !empty($user->userName)) {
-                header("location: " . HOSTNAME);
             }
         }
     }
