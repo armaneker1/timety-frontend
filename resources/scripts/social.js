c6795f6dd782341e89a70508ad7a1f56d600971e
hasan <hasan@mm>
2013-05-08 11:33:47 +0300

    campaign user ekledi

--- resources/scripts/social.js
+++ resources/scripts/social.js
@@ -56,7 +56,7 @@ function shareThisFacebook()
 function shareThisTwitter(header)
 {
     var u=location.href;
-    window.open('http://twitter.com/share?url='+encodeURIComponent(u)+'&text='+header+' by @mytimety&count=horiztonal','sharertw','toolbar=0,status=0,width=626,height=436');
+    window.open('http://twitter.com/share?url='+encodeURIComponent(u)+'&text='+encodeURIComponent(header+' by @mytimety&count=horiztonal'),'sharertw','toolbar=0,status=0,width=626,height=436');
     return false;
 }
 
